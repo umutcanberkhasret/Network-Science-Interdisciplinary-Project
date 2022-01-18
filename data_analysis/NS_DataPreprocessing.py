09609718a63e0c7dbaf888b62ed63b4c0d610c03
Umutcan Berk Hasret <38487394+umutcanberkhasret@users.noreply.github.com>
2022-01-18 19:24:40 +0100

    Preprocess method is modified to add the "Entities" field to model

--- data_analysis/NS_DataPreprocessing.py
+++ data_analysis/NS_DataPreprocessing.py
@@ -9,13 +9,23 @@ import pandas as pd
 import re
 import string
 import ast
+import enum
+
+
+class CountMode(enum.Enum):
+    count_within_all_tweets = 1
+    count_within_verified_tweets = 2
+    count_within_regular_tweets = 3
 
 
 def preProcessData(df):
     for index, row in df.iterrows():
-        # get full_text of the tweet
+        # get full_text and entities(hashtags mentions etc.) of the tweet
         if row['truncated']:
-            tweet = {"full_text": ast.literal_eval(row['extended_tweet'])['full_text']}
+            tweet = {
+                "full_text": ast.literal_eval(row['extended_tweet'])['full_text'],
+                "entities": ast.literal_eval(row['extended_tweet'])['entities']
+            }
         else:
             # if the tweet is retweeted, this status will not be na
             if ('retweeted_status' in row) and (not pd.isna(row['retweeted_status'])):
@@ -24,16 +34,25 @@ def preProcessData(df):
 
                 # if the text is truncated
                 if originalTweet['truncated']:
-                    tweet = {"full_text": "RT @" + userTagName + ": " + originalTweet['extended_tweet']['full_text']}
+                    tweet = {
+                        "full_text": "RT @" + userTagName + ": " + originalTweet['extended_tweet']['full_text'],
+                        "entities": originalTweet['extended_tweet']['entities']
+                    }
                 else:
-                    tweet = {"full_text": "RT @" + userTagName + ": " + originalTweet['text']}
+                    tweet = {
+                        "full_text": "RT @" + userTagName + ": " + originalTweet['text'],
+                        "entities": originalTweet['entities']
+                    }
 
                 del originalTweet
                 del userTagName
 
             # if the tweet is original and not truncated
             else:
-                tweet = {"full_text": row['text']}
+                tweet = {
+                    "full_text": row['text'],
+                    "entities": row['entities']
+                }
 
         # get Username & User Tag & Location & Verification status of account
         tweet["username"] = ast.literal_eval(row['user'])['name']
@@ -53,27 +72,35 @@ def preProcessData(df):
         # get TweetID
         tweet["tweet_id"] = row['id']
 
-        # add to the list
-        if not pd.isna(tweet['location']):
-            tweets.append(tweet)
+        tweets.append(tweet)
 
         del tweet
         del row
         del index
 
 
-def countNumberOfRetweetedTweets(tweets):
+def countNumberOfRetweetedTweets(tweets, countMode):
     # Separate RTs and original tweets
     for tweet in tweets:
         if bool(re.match(r'(RT @[\w]+:)', tweet["full_text"])):
-            rts.append(tweet)
+            if countMode == CountMode.count_within_all_tweets:
+                rts.append(tweet)
+            elif countMode == CountMode.count_within_verified_tweets:
+                verified_rts.append(tweet)
+            elif countMode == CountMode.count_within_regular_tweets:
+                regular_rts.append(tweet)
         else:
-            non_rt.append(tweet)
+            if countMode == CountMode.count_within_all_tweets:
+                non_rt.append(tweet)
+            elif countMode == CountMode.count_within_verified_tweets:
+                verified_non_rt.append(tweet)
+            elif countMode == CountMode.count_within_regular_tweets:
+                regular_non_rt.append(tweet)
         del tweet
 
 
+# Distinguish tweets according to their account verification status
 def countTweetsFromVerifiedAccounts(tweets):
-    # Distinguish tweets according to their account verification status    
     for tweet in tweets:
         if tweet["is_verified"]:
             tweets_from_verified_accounts.append(tweet)
@@ -114,6 +141,10 @@ tweets_from_regular_accounts = []
 
 rts = []
 non_rt = []
+verified_rts = []
+regular_rts = []
+verified_non_rt = []
+regular_non_rt = []
 
 # preprocess all the given hashtags and build 
 for df_instance in dfs:
@@ -121,8 +152,16 @@ for df_instance in dfs:
 
     del df_instance
 
-countNumberOfRetweetedTweets(tweets)
 countTweetsFromVerifiedAccounts(tweets)
+
+# total number of retweeted all tweets
+countNumberOfRetweetedTweets(tweets, CountMode.count_within_all_tweets)
+
+# total number of retweeted only verified tweets
+countNumberOfRetweetedTweets(tweets_from_verified_accounts, CountMode.count_within_verified_tweets)
+
+# total number of retweeted only regular tweets
+countNumberOfRetweetedTweets(tweets_from_regular_accounts, CountMode.count_within_regular_tweets)
 
 original_tweets_from_pakistan = filterLocations(non_rt, pakistanCities)
 all_tweets_from_pakistan = filterLocations(tweets, pakistanCities)
